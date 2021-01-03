29fccfe3987dab02d4dc0424ce6a024ccbf9a2b4
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-03 15:59:08 -0800

    Mixin field is now @Unique

--- src/main/java/io/github/redstoneparadox/creeperfall/mixin/CreeperEntityMixin.java
+++ src/main/java/io/github/redstoneparadox/creeperfall/mixin/CreeperEntityMixin.java
@@ -4,14 +4,19 @@ import io.github.redstoneparadox.creeperfall.hooks.CreeperHooks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreeperEntity.class)
-public class CreeperEntityMixin extends HostileEntity implements CreeperHooks {
+public abstract class CreeperEntityMixin extends HostileEntity implements CreeperHooks {
+	@Unique
 	private boolean isCreeperfallCreeper = false;
 
 	protected CreeperEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
