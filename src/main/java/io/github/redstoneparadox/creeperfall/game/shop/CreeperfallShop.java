ed319fc053a89075617f817db5cf1db0f5e1fbaa
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-03 21:08:43 -0800

    Tried and failed to add lore to the guardian spawn egg

--- src/main/java/io/github/redstoneparadox/creeperfall/game/shop/CreeperfallShop.java
+++ src/main/java/io/github/redstoneparadox/creeperfall/game/shop/CreeperfallShop.java
@@ -3,15 +3,26 @@ package io.github.redstoneparadox.creeperfall.game.shop;
 import io.github.redstoneparadox.creeperfall.game.CreeperfallActive;
 import io.github.redstoneparadox.creeperfall.item.CreeperfallItems;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
 import xyz.nucleoid.plasmid.shop.Cost;
 import xyz.nucleoid.plasmid.shop.ShopUi;
 
 public class CreeperfallShop {
 	public static ShopUi create(ServerPlayerEntity player, CreeperfallActive game) {
 		return ShopUi.create(new LiteralText("Shop"), shop -> {
-			shop.addItem(new ItemStack(CreeperfallItems.GUARDIAN_SPAWN_EGG), Cost.ofEmeralds(1));
+			shop.addItem(guardianSpawnEgg(), Cost.ofEmeralds(1));
 		});
+	}
+
+	private static ItemStack guardianSpawnEgg() {
+		ItemStack stack = new ItemStack(CreeperfallItems.GUARDIAN_SPAWN_EGG);
+		CompoundTag nbt = stack.getOrCreateTag();
+
+		nbt.putString("Lore", new TranslatableText("item.creeperfall.guardian_spawn_egg.lore").asString());
+
+		return stack;
 	}
 }
