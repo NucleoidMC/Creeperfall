c3dd3630afe7b6f94dad11a3ab81b0525a5c20de
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-04 19:51:30 -0800

    Created EntityTracker for easy entity tracking

---
+++ src/main/java/io/github/redstoneparadox/creeperfall/game/util/EntityTracker.java
@@ -0,0 +1,30 @@
+package io.github.redstoneparadox.creeperfall.game.util;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class EntityTracker {
+	private Map<EntityType<?>, Set<Entity>> entityMap = new HashMap<>();
+
+	public void add(Entity entity) {
+		EntityType<?> type = entity.getType();
+		Set<Entity> set = entityMap.computeIfAbsent(type, entityType -> new HashSet<>());
+		set.add(entity);
+	}
+
+	public void remove(Entity entity) {
+		EntityType<?> type = entity.getType();
+		Set<Entity> set = entityMap.computeIfAbsent(type, entityType -> new HashSet<>());
+		set.remove(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public <E extends Entity> Set<E> getAll(EntityType<E> type) {
+		return (Set<E>) entityMap.computeIfAbsent(type, entityType -> new HashSet<>());
+	}
+}
