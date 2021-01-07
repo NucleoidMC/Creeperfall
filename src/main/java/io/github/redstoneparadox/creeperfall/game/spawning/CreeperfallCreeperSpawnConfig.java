8f70255ec6d6cfdb29cc95f2f7278902c0c05d35
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-07 14:23:29 -0800

    Separated number of stages from the additional amount of creepers per stage
    Gives more control over game pacing since creeper increment is a double

--- src/main/java/io/github/redstoneparadox/creeperfall/game/spawning/CreeperfallCreeperSpawnConfig.java
+++ src/main/java/io/github/redstoneparadox/creeperfall/game/spawning/CreeperfallCreeperSpawnConfig.java
@@ -5,22 +5,25 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public class CreeperfallCreeperSpawnConfig {
 	public static final Codec<CreeperfallCreeperSpawnConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			Codec.INT.fieldOf("max_creepers_per_player").forGetter(config -> config.maxCreepersPerPlayer),
+			Codec.INT.fieldOf("stages").forGetter(config -> config.stages),
 			Codec.INT.fieldOf("stage_length_secs").forGetter(config -> config.stageLengthSeconds),
+			Codec.DOUBLE.fieldOf("spawn_count_increment").forGetter(config -> config.spawnCountIncrement),
 			Codec.INT.fieldOf("creeper_spawn_delay_secs").forGetter(config -> config.creeperSpawnDelaySeconds),
 			Codec.INT.fieldOf("creeper_spawn_height").forGetter(config -> config.creeperSpawnHeight),
 			Codec.doubleRange(0.0, 1.0).fieldOf("creeper_fall_speed_multiplier").forGetter(config -> config.creeperFallSpeedMultiplier)
 	).apply(instance, CreeperfallCreeperSpawnConfig::new));
 
-	public final int maxCreepersPerPlayer;
+	public final int stages;
 	public final int stageLengthSeconds;
+	public final double spawnCountIncrement;
 	public final int creeperSpawnDelaySeconds;
 	public final int creeperSpawnHeight;
 	public final double creeperFallSpeedMultiplier;
 
-	public CreeperfallCreeperSpawnConfig(int maxCreepersPerPlayer, int stageLengthSeconds, int creeperSpawnDelaySeconds, int creeperSpawnHeight, double creeperFallSpeedMultiplier) {
-		this.maxCreepersPerPlayer = maxCreepersPerPlayer;
+	public CreeperfallCreeperSpawnConfig(int stages, int stageLengthSeconds, double spawnCountIncrement, int creeperSpawnDelaySeconds, int creeperSpawnHeight, double creeperFallSpeedMultiplier) {
+		this.stages = stages;
 		this.stageLengthSeconds = stageLengthSeconds;
+		this.spawnCountIncrement = spawnCountIncrement;
 		this.creeperSpawnDelaySeconds = creeperSpawnDelaySeconds;
 		this.creeperSpawnHeight = creeperSpawnHeight;
 		this.creeperFallSpeedMultiplier = creeperFallSpeedMultiplier;
