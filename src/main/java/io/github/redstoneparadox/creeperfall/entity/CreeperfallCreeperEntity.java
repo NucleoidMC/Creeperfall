d7f1c0f4ec0487f7b55307cbd3988959054889e0
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-07 15:29:11 -0800

    Tried to make creepers target skeletons to no avail

--- src/main/java/io/github/redstoneparadox/creeperfall/entity/CreeperfallCreeperEntity.java
+++ src/main/java/io/github/redstoneparadox/creeperfall/entity/CreeperfallCreeperEntity.java
@@ -1,7 +1,20 @@
 package io.github.redstoneparadox.creeperfall.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.CreeperIgniteGoal;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
+import net.minecraft.entity.ai.goal.FollowTargetGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.RevengeGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.OcelotEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -17,6 +30,22 @@ public class CreeperfallCreeperEntity extends CreeperEntity {
 		this(world);
 		this.fallSpeedMultiplier = fallSpeedMultiplier;
 		this.experiencePoints = 0;
+	}
+
+	@Override
+	protected void initGoals() {
+		super.initGoals();
+		this.goalSelector.add(1, new SwimGoal(this));
+		this.goalSelector.add(2, new CreeperIgniteGoal(this));
+		this.goalSelector.add(3, new FleeEntityGoal<>(this, OcelotEntity.class, 6.0F, 1.0D, 1.2D));
+		this.goalSelector.add(3, new FleeEntityGoal<>(this, CatEntity.class, 6.0F, 1.0D, 1.2D));
+		this.goalSelector.add(4, new MeleeAttackGoal(this, 1.0D, false));
+		this.goalSelector.add(5, new WanderAroundFarGoal(this, 0.8D));
+		this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+		this.goalSelector.add(6, new LookAroundGoal(this));
+		this.targetSelector.add(1, new FollowTargetGoal<>(this, PlayerEntity.class, true));
+		this.goalSelector.add(6, new LookAtEntityGoal(this, SkeletonEntity.class, 8.0F));
+		this.targetSelector.add(1, new FollowTargetGoal<>(this, SkeletonEntity.class, true));
 	}
 
 	@Override
