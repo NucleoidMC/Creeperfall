01613d11ef4d1dfc95e0661bb3653244cd35a913
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-30 15:00:10 -0800

    Ocelots no longer kill all creepers, just the ones near them

--- src/main/java/io/github/redstoneparadox/creeperfall/entity/CreeperfallOcelotEntity.java
+++ src/main/java/io/github/redstoneparadox/creeperfall/entity/CreeperfallOcelotEntity.java
@@ -1,7 +1,10 @@
 package io.github.redstoneparadox.creeperfall.entity;
 
+import io.github.redstoneparadox.creeperfall.entity.ai.goal.CreeperfallFollowTargetGoal;
 import io.github.redstoneparadox.creeperfall.game.util.EntityTracker;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.FollowTargetGoal;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.passive.OcelotEntity;
 import net.minecraft.util.math.Vec3d;
@@ -11,7 +14,7 @@ import java.util.Set;
 
 public class CreeperfallOcelotEntity extends OcelotEntity {
 	private EntityTracker tracker;
-	private int timeToDespawn = 3 * 20;
+	private int timeToDespawn = 15 * 20;
 
 	@Deprecated
 	public CreeperfallOcelotEntity(World world) {
@@ -24,26 +27,26 @@ public class CreeperfallOcelotEntity extends OcelotEntity {
 	}
 
 	@Override
-	public void setMovementSpeed(float movementSpeed) {
-
-	}
-
-	@Override
-	public void setVelocity(Vec3d velocity) {
-
+	protected void initGoals() {
+		this.targetSelector.add(1, new CreeperfallFollowTargetGoal<>(this, CreeperEntity.class, 10, false, false, Entity::isOnGround));
 	}
 
 	@Override
 	public void tick() {
+		Set<CreeperEntity> creepers = tracker.getAll(EntityType.CREEPER);
+
+		for (CreeperEntity creeper: creepers) {
+			if (getPos().distanceTo(creeper.getPos()) <= 16 && creeper.isOnGround()) {
+				creeper.kill();
+			}
+			else if (getPos().distanceTo(creeper.getPos()) <= 4) {
+				creeper.kill();
+			}
+		}
+
 		timeToDespawn -= 1;
 
 		if (timeToDespawn <= 0 && !removed) {
-			Set<CreeperEntity> creepers = tracker.getAll(EntityType.CREEPER);
-
-			for (CreeperEntity creeperEntity: creepers) {
-				creeperEntity.kill();
-			}
-
 			remove();
 		}
 
